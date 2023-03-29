c11365d651bbda9cff4608ec6ae5be8aa6bda07d
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-03-29 22:27:12 +0300

    Customized admin panel in admin.py

--- foodie/admin.py
+++ foodie/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from foodie.models import Dish, DishType, Cook
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "dish_type"]
+    list_filter = ["dish_type"]
+    search_fields = ["name"]
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info",
+         {"fields": ("first_name", "last_name", "years_of_experience")}),
+    )
