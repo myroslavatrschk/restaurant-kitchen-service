c2c7dbf7615b4a626cacf1ea146d05cd42da2eb8
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-03-30 16:52:34 +0300

    Added basic templates to the pages: Cooks, Dishes, Dish types

--- foodie/urls.py
+++ foodie/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from foodie.views import index
+from foodie.views import (
+    index,
+    DishListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView,
+    DishTypeListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dishes/", DishListView.as_view(), name="dishes"),
+    path("dishes/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("cooks/", CookListView.as_view(), name="cooks"),
+    path("cooks/<int:pk>", CookDetailView.as_view(), name="cook-detail"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "foodie"
--- foodie/views.py
+++ foodie/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from foodie.models import Dish, DishType, Cook
 
@@ -16,3 +17,31 @@ def index(request):
     }
 
     return render(request, "foodie/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "foodie/dish_type_list.html"
+    queryset = DishType.objects.order_by("name")
+    paginate_by = 10
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 10
+    queryset = Dish.objects.select_related("dish_type")
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 10
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.prefetch_related("dishes__dish_type")
