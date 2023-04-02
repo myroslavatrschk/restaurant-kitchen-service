a45a7eccbe8d51f402bc205f1cf2aff944dd187b
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-04-03 01:55:14 +0300

    Removed insecured Django secret key from settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-*x-rukstd(e62z8f0w)+2&%5%6nk=%a@*vvmsci2t$bg2j=wsk")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
